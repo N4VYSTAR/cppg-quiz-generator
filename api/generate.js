7946cd55193d317d6cc73a76a418fb2dedb1f050
N4VYSTAR <n4vystar@gmail.com>
2026-08-03 15:02:57 +0900

    모델명 변경 gemini 2.5 flash

--- api/generate.js
+++ api/generate.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
 try {
     const apiKey = process.env.GEMINI_API_KEY; // 환경변수에서 키 읽기
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
     {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
